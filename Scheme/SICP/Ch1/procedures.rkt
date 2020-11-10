e016da46c2c1f2248d664a2273d7aae793d40996
AlexBennett5 <bnet505@gmail.com>
2020-11-10 01:23:07 -0600

    Finished chapter 1

--- Scheme/SICP/Ch1/procedures.rkt
+++ Scheme/SICP/Ch1/procedures.rkt
@@ -241,4 +241,33 @@
 (define (average-damp f)
   (lambda (x) (average x (f x))))
 
+(define dx 0.00001)
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
 
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+(define (newton-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cube x) (* x x x))
+(define (cubic a b c)
+  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1) f (compose f (repeated f (dec n)))))
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)))
+
+(define (repeated-smooth f n)
+  ((repeated smooth n) f))
+
+(define (iterative-improve check improve)
+  (lambda (x) (if (check x) x ((iterative-improve check improve) (improve x)))))
