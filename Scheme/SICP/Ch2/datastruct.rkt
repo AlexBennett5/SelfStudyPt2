6c683e281c35b2cff8d08d9e610d7a82049dff70
AlexBennett5 <bnet505@gmail.com>
2020-11-13 00:54:08 -0600

    Continuing Ch2

--- Scheme/SICP/Ch2/datastruct.rkt
+++ Scheme/SICP/Ch2/datastruct.rkt
@@ -87,3 +87,44 @@
   (* (length-segment (make-segment (rect-ul r) (rect-ur r)))
      (length-segment (make-segment (rect-ul r) (rect-ll r)))))
 
+;;; Interval Arithmetic
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound inv) (car inv))
+(define (upper-bound inv) (cdr inv))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x)
+                    (upper-bound y))
+                 (- (upper-bound x)
+                    (lower-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+  (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+    (make-interval (/ 1.0 (upper-bound y)) (/ 1.0 (lower-bound y)))))
+
+;;; Misc
+
+(define (cons-expt a b)
+  (* (expt 2 a)
+     (expt 3 b)))
+
+(define (log-reduce n base)
+  (cond ((not (= (remainder n base) 0)) 0)
+        (else (+ (log-reduce (/ n base) base) 1))))
+
+(define (car-expt num)
+  (log-reduce num 2))
+(define (cdr-expt num)
+  (log-reduce num 3))
