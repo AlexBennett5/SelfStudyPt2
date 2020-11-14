e7f196e666067a5c1355b27707e6acff0f157911
AlexBennett5 <bnet505@gmail.com>
2020-11-14 00:59:45 -0600

    Working on parser

--- C/http-server/request_parse.c
+++ C/http-server/request_parse.c
@@ -6,13 +6,24 @@ http_req_info parse_http_req(char* request_text) {
 
   char* filename;
 
-  // Allocate memory to filename and check it isn't NULL
-  
+  if ((filename = malloc(sizeof(char) * strlen(request_text))) == NULL)
+    //ERROR: Allocation failure
+
   filename = get_filename(request_text);
+  strcpy(info.filename, filename);
 
-  // Check if they asked for / and give them index.html
+  if (strcmp(filename, "/test-site/") == 0) {
+    strcat(info.filename, "index.html"); 
+  }
 
-  // Check if file exists
+  struct stat buffer;
+  
+  if (!stat(filename, &buffer)) {
+    info.returncode = 404;
+    info.filename = "/test-site/404.html";
+  } else {
+    info.returncode = 200;
+  }
 
   return info;
 }
@@ -20,18 +31,20 @@ http_req_info parse_http_req(char* request_text) {
 char* get_filename(char* request_text) {
   char* filename;
 
-  // Allocate memory for filename
+  if ((filename = malloc(sizeof(char) * strlen(request_text))) == NULL)
+    //ERROR: Allocation failure
 
-  // sscanf() to get filename from header
+  sscanf(request_text, "GET %s HTTP/1.1", filename);
 
   char* filename_w_path;
 
-  // Allocate memory for filename_w_path
+  if ((filename_w_path = malloc(sizeof(char) * (12 + strlen(filename)))) == NULL)
+    //ERROR: Allocation failure
 
-  char* folder_name = "";
+  char* folder_name = "/test-site";
 
-  // strcpy, strcat to create filename_w_path
-  
+  strcpy(filename_w_path, folder_name);
+  strcat(filename_w_path, filename);
   free(filename);
   return filename_w_path;
 }
--- C/http-server/response_constructor.c
+++ C/http-server/response_constructor.c
@@ -1,28 +1,47 @@
 #include <stdlib>
 #include "response_constructor.h"
 
-int send_msg(int fd, char *msg) {
+char *response_200 = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
+char *response_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html\n\n";
 
+int send_msg(int fd, char *msg) {
+  return write(fd, msg, strlen(msg));
 }
 
 int send_header(int fd, int returncode) {
-
+  switch(returncode) {
+    case 404:
+      send_msg(fd, response_404);
+      break;
+    case 200;
+      send_msg(fd, response_200);
+      break;
+  }
 }
 
 int send_body(int fd, char* filename) {
 
   FILE *file;
 
-  // Open file with fopen() and check for error
+  if ((file = fopen(filename, "r")) == NULL)
+    //ERROR: Can't open
 
-  // Get total size of file using stat() and struct stat
-
+  struct stat buffer;
+  int size = stat(filename, &buffer);
+  
   char *line;
 
-  // Allocate space to store each line of file
+  if ((line = malloc(size)) == NULL)
+    //ERROR: Allocating line
 
-  // Use getline() to get data and send_msg() the line
+  int end_of_file;
+  size_t line_size = 1
 
-  // Return how big the file was
+  while ((end_of_file = getline(&line, &line_size, file)) > 0)
+    send_msg(fd, line);
+
+  send_msg(fd, "\n");
+  free(line);
+  return size;
 }
 
